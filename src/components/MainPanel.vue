fb96dd34e00c16079c39e22a54d69226f4f76180
695954085 <695954085@qq.com>
2020-04-13 23:28:33 +0800

    feat: initial experience chartjs

---
+++ src/components/MainPanel.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-container>
+    <el-header>佛山房屋销售曲线图</el-header>
+    <el-main>
+      <curve-component :chartdata="chartData" />
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import CurveComponent from './CurveComponent'
+
+export default {
+  components: {
+    CurveComponent
+  },
+  data () {
+    return {
+      chartData: {
+        labels: ['January', 'February'],
+        datasets: [
+          {
+            label: 'Data One',
+            data: [40, 20]
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
