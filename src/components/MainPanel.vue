50a190e6ecbf7dd91ab75ad240a330b3e127b95d
695954085 <695954085@qq.com>
2020-04-14 23:31:32 +0800

    feat: learning how to config fontSize

--- src/components/MainPanel.vue
+++ src/components/MainPanel.vue
@@ -2,28 +2,79 @@
   <el-container>
     <el-header>佛山房屋销售曲线图</el-header>
     <el-main>
-      <curve-component :chartdata="chartData" />
+      <curve-component :chartdata="chartData" style="width: 500px;height:500px"/>
+      <bar-component :chartdata="barChartData" :options="barOptions" aria-label="Hello ARIA World" style="width: 1000px;height:1000px"/>
     </el-main>
   </el-container>
 </template>
 
 <script>
 import CurveComponent from './CurveComponent'
-
+import BarComponent from './BarComponent'
+Chart.defaults.global.defaultFontSize = 24
 export default {
   components: {
-    CurveComponent
+    CurveComponent,
+    BarComponent
   },
   data () {
     return {
       chartData: {
-        labels: ['January', 'February'],
-        datasets: [
-          {
-            label: 'Data One',
-            data: [40, 20]
-          }
-        ]
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      barChartData: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      barOptions: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
       }
     }
   }
